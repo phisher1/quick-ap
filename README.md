2265784254c1124833135bef325afd69e772de0d
phisher1 <phisher1@gmail.com>
2016-09-14 10:19:15 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# quick-ap
+
+This is just a tiny bash script that installs and uses hostapd and dnsmasq 
+to spin up a wireless AP. 
+
+It also supports setting a WEP key and changing your MAC address using macchanger.
+
+Do with this what you will.
+
---
+++ apd.sh
@@ -0,0 +1,139 @@
+#!/bin/bash
+## quick and dirty AP with hostapd and dnsmasq
+## exit properly with ctrl-c
+
+echo "Exit this script with Ctrl-C and it will attempt to clean up properly."
+
+if [ -z $1 ]; then
+   echo -n "SSID: "
+   read ssid
+else
+   ssid=$1
+fi
+
+# get wep key
+function get_wep_key() { 
+	echo -n "WEP Key [must be exactly 5 or 13 ascii characters]: " 
+	read wep_key
+	if [[  $wep_key =~ ^[a-zA-Z0-9]{5}$ ]] ; then
+	   echo "Key accepted"
+   elif [[  $wep_key =~ ^[a-zA-Z0-9]{13}$ ]] ; then
+		echo "Key accepted"
+	else
+		echo "Wep key must be exactly 5 or 13 characters"
+		get_wep_key
+	fi
+}
+
+# get mac
+function get_mac() {
+	echo -n "Enter MAC address in the following format AB:CD:EF:12:34:56: "
+	read new_mac
+	if [[  $new_mac =~ ^[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}:[a-zA-Z0-9]{2}$ ]] ; then
+		macchanger --mac=$new_mac wlan0
+   else
+		echo "MAC Address format not correct."
+		get_mac
+	fi
+}
+
+echo -n "Do you want WEP enabled? [y/n]: "
+read wep
+case $wep in
+	y*)
+		get_wep_key
+	;;
+	*)
+	;;
+esac
+
+echo -n "Do you want to change your MAC? [y/n]: "
+read changemac
+case $changemac in
+	y*)
+		echo -n "Random MAC? [y/n]: "
+      read random_mac
+		case $random_mac in
+			y*)
+				macchanger -r wlan0
+			;;
+			n*)
+				get_mac
+			;;
+			*)
+				echo "You didn't type y or n. Assuming n."
+			;;
+		esac
+	;;
+   n*)
+	;;
+esac
+
+
+
+
+# install packages if need be
+if [ $(dpkg-query -W -f='${Status}' dnsmasq 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  apt-get install dnsmasq
+fi
+if [ $(dpkg-query -W -f='${Status}' hostapd 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  apt-get install hostapd
+fi
+
+# trap control c
+trap ctrl_c INT
+
+function ctrl_c() {
+   echo "wlan0 managed mode"
+   iwconfig wlan0 mode managed
+   echo "downing wlan0"
+   ifconfig wlan0 down
+   echo "flushing firewall"
+   iptables -F
+   iptables -F -t nat
+   echo "resetting wlan0 mac"
+   macchanger -p wlan0
+   kill -9 `cat /tmp/dnsmasq.run`
+}
+
+
+## script begins
+
+# stop and disable services
+service hostapd stop
+service dnsmasq stop
+pkill -9 dnsmasq
+pkill -9 hostapd
+
+# bring up wlan0
+rfkill unblock wlan
+iwconfig wlan0 mode monitor
+ifconfig wlan0 10.0.0.1/24 up
+
+# forwarding and nat
+sysctl net.ipv4.ip_forward=1
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+# dns masq
+cat > /tmp/dnsmasq.conf <<!
+bind-interfaces
+interface=wlan0
+dhcp-range=10.0.0.2,10.0.0.254
+!
+
+# hostapd
+cat > /tmp/hostapd.conf<<!
+interface=wlan0
+driver=nl80211
+ssid=${ssid}
+hw_mode=g
+channel=6
+!
+
+
+if [[ -n $wep_key ]]; then echo -e "wep_default_key=0\nwep_key0=\"${wep_key}\"" >> /tmp/hostapd.conf; fi
+
+dnsmasq --pid-file=/tmp/dnsmasq.run -C /tmp/dnsmasq.conf
+hostapd /tmp/hostapd.conf
